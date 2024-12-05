961ccc20c3aac14222a6431993e4f3ae20474561
Aarjav Jain <anshjain638@gmail.com>
2024-12-05 21:07:53 +0530

    added test cases

---
+++ App.test.tsx
@@ -0,0 +1,144 @@
+import {
+  act,
+  fireEvent,
+  render,
+  screen,
+  userEvent,
+  waitFor,
+} from '@testing-library/react-native';
+import App from './App';
+
+const mockFuntion = jest.fn();
+
+// mocking modules
+
+jest.mock('react-native/Libraries/Alert/Alert', () => {
+  return {
+    alert: jest.fn(),
+  };
+});
+
+describe('App Component', () => {
+  test('renders correctly', async () => {
+    jest.useFakeTimers();
+    //setup the user event
+
+    const user = userEvent.setup({
+      advanceTimers: () => jest.advanceTimersByTime(10000),
+    });
+
+    render(<App age={20} />);
+
+    let ageElement = screen.getByTestId('age');
+    expect(ageElement).toHaveTextContent('Age: 20');
+    // force update the component not working
+    // screen.update(<App age={60} key={'new-key'}/>);
+    // screen.debug();
+    // await waitFor(() => {
+    //   ageElement = screen.getByTestId('age');
+    //   expect(ageElement).toHaveTextContent('Age: 60');
+    // });
+    screen.update(<App age={60} onPress={mockFuntion} />);
+    expect(ageElement).toHaveTextContent('Age: 60');
+    expect(screen.getByText('App')).toBeTruthy();
+    expect(screen.getByLabelText('CheckText')).toBeTruthy();
+    expect(screen.getByText('App')).toBeOnTheScreen();
+    expect(screen.getByLabelText('CheckText')).toBeOnTheScreen();
+
+    const testElement = screen.queryByText('Test');
+    expect(testElement).toBeTruthy();
+
+    // Uncomment the below code to see the result when element is not found
+
+    // below code will throw error as element is not found and it will wait for 2 seconds
+
+    // const findElement = await screen.findByText('Find', {
+    //   exact: true,
+    // }, {
+    //   timeout: 2000,
+    //   interval: 100,
+    //   onTimeout:(error)=>{
+    //     screen.debug({
+    //       message: 'Element not found',
+    //       mapProps: (props) => {
+    //         return {
+    //           ...props,
+    //           children: 'Element not found',
+    //         };
+    //       }
+    //     });
+    //     return error;
+    //   }
+
+    // });
+
+    const findElement = await screen.findByText(
+      'Find By',
+      {
+        exact: true,
+      },
+      {
+        timeout: 2000,
+        interval: 100,
+        onTimeout: error => {
+          screen.debug({
+            message: 'Element not found',
+            mapProps: props => {
+              return {
+                ...props,
+                children: 'Element not found',
+              };
+            },
+          });
+          return error;
+        },
+      },
+    );
+    expect(findElement).toBeTruthy();
+
+    await waitFor(
+      () => {
+        expect(screen.getByTestId('Count')).toHaveTextContent('28');
+      },
+      {
+        timeout: 3000,
+        interval: 1000,
+        onTimeout: error => {
+          console.error('Timeout Error: Element not updating');
+          screen.debug();
+          throw error;
+        },
+      },
+    );
+
+    expect(screen.getByTestId('Count')).toBeOnTheScreen();
+
+    //user Event
+    const button = screen.getByTestId('button');
+
+    await user.press(button);
+
+    const textConditional = screen.queryByTestId('visibility');
+
+    expect(textConditional).toHaveTextContent('Visible');
+
+    //FIRE EVENT API
+
+    // fireEvent.press(button);
+    // act(() => {
+    //   jest.advanceTimersByTime(10000);
+    // });
+
+    // const textConditional1 = screen.queryByTestId('visibility');
+
+    // expect(textConditional1).toHaveTextContent('Visible');
+
+    //mock function
+
+    const button1 = screen.getByTestId('button1');
+
+    await user.press(button1);
+
+    expect(mockFuntion).toHaveBeenCalled();
+  });
+});
--- App.tsx
+++ App.tsx
@@ -1,118 +1,101 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
+  View,
   Text,
   useColorScheme,
-  View,
+  StatusBar,
+  Alert,
+  Button,
 } from 'react-native';
+import React, {useEffect} from 'react';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = ({age, onPress}: {age: number; onPress?: () => void}) => {
+  useEffect(() => {
+    return () => {
+      console.log('Age:', age);
+    };
+  }, []);
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const [Count, setCount] = React.useState(30);
+
+  const [time, setTime] = React.useState(new Date().toLocaleTimeString());
+
+  const [visiblity, setVisiblity] = React.useState(false);
+
+  const handleUpdate = () => {
+    setTimeout(() => {
+      setVisiblity(!visiblity);
+    }, 5000);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      updateTime();
+      updateCount();
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  const updateCount = () => {
+    setCount(prevCount => {
+      const newCount = prevCount != 0 ? prevCount - 1 : 0;
+      if (newCount === 0) {
+        Alert.alert('30 Seconds are over', 'Resetting the counter');
+        setCount(30);
+      }
+      return newCount;
+    });
+  };
+
+  const updateTime = () => {
+    setTime(new Date().toLocaleTimeString());
+  };
+
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <View
+      style={{
+        ...backgroundStyle,
+        flex: 1,
+        padding: 20,
+        rowGap: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+      <Text>App</Text>
+
+      <Text accessibilityLabel="CheckText">Checking queries</Text>
+
+      <Text>Test</Text>
+
+      <Text>Find By</Text>
+
+      <Text testID="Count">{Count}</Text>
+
+      <Text>TIme: {time}</Text>
+
+      <Text testID="age">Age: {age ?? 20}</Text>
+
+      <Button title="Click me" testID="button" onPress={handleUpdate} />
+
+      {visiblity && <Text testID="visibility">Visible</Text>}
+
+      <Button title="Click me" testID="button1" onPress={onPress} />
+    </View>
+  );
+};
 
 export default App;
